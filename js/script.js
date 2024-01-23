df4847e4549a7c7f6edbb435109cc9f3d15d2620
Rafael <Rafael.mattos468@gmail.com>
2024-01-23 19:29:27 -0300

    Usando o fetch para intercalar as paginas com um melhor desenpenho

--- js/script.js
+++ js/script.js
@@ -1,48 +1,80 @@
-function abriPage(a){
-    let localPag = document.querySelector('body')
-    let pag = new XMLHttpRequest()
+// function abriPage(a){
+//     let localPag = document.querySelector('body')
+//     let pag = new XMLHttpRequest()
 
-    pag.onreadystatechange = () => {
-        if(pag.readyState == 4 && pag.status == 200){
-            localPag.innerHTML = pag.response
-        }
-    }
+//     pag.onreadystatechange = () => {
+//         if(pag.readyState == 4 && pag.status == 200){
+//             localPag.innerHTML = pag.response
+//         }
+//     }
 
-    pag.open('GET', `../${a}.html`)
-    pag.send()
-}
+//     pag.open('GET', `../${a}.html`)
+//     pag.send()
+// }
+
+let pages = ['app1.html', 'app2.html'];
+let currentPage = 0;
+
+document.querySelector('#botao').addEventListener('click', function () { // nesse evento usando fetch o body do app1 toma o lugar do body do index. 
+    currentPage = (currentPage + 1) % pages.length
+    fetch(pages[currentPage])
+        .then(response => response.text())
+        .then(data => {
+            let parser = new DOMParser();
+            let htmlDocument = parser.parseFromString(data, 'text/html');
+            let divContent = htmlDocument.querySelector('main').innerHTML;
+            document.querySelector('main').innerHTML = divContent;
+        })
+        .catch(error => {
+            console.error('erro:', error);
+        })
+})
+
+// document.querySelector('#botao1').addEventListener('click', function() {
+//     fetch('app2.html')
+//         .then(response => response.text())
+//         .then(data => {
+//             let parser = new DOMParser();
+//             let htmlDocument = parser.parseFromString(data, 'text/html');
+//             let divContent = htmlDocument.querySelector('body').innerHTML;
+//             document.querySelector('body').innerHTML = divContent;
+//         })
+//         .catch(error => {
+//             console.error('Erro:', error);
+//         });
+// });
 
 
-function calcGasto(){
+function calcGasto() {
     const percurso = Number(document.getElementById('km').value)
     const mediaVeiculo = Number(document.getElementById('media-veiculo').value)
     const valorLitro = Number(document.getElementById('valor-litro').value)
 
     let gasto = (percurso / mediaVeiculo) * valorLitro;
 
-    
-    if (isNaN(gasto)){
+
+    if (isNaN(gasto)) {
         document.getElementById('result-media').innerHTML = `Prencha os capos para ter o resultado`
-        document.querySelector('body').style.background = `darkred` 
-    }else{
+        document.querySelector('body').style.background = `darkred`
+    } else {
         document.getElementById('result-media').innerHTML = `<span>R$${gasto.toFixed(1)}</span> de gasto com combustivel`
-        document.querySelector('body').style.background = `darkblue` 
+        document.querySelector('body').style.background = `darkblue`
     }
 
 }
 
-function calcMedia(){
+function calcMedia() {
     const km = Number(document.getElementById('km').value)
     const litros = Number(document.getElementById('litros').value)
     let media = km / litros;
-  
-    if (isNaN(media)){
+
+    if (isNaN(media)) {
         document.getElementById('result-media').innerHTML = `Prencha os capos para ter a média de consumo`
-        document.querySelector('body').style.background = `darkred` 
-    }else{
+        document.querySelector('body').style.background = `darkred`
+    } else {
         document.getElementById('result-media').innerHTML = `<span>${media.toFixed(2)}</span> Quilômetros por Litro`
-        document.querySelector('body').style.background = `darkblue` 
+        document.querySelector('body').style.background = `darkblue`
     }
 
-    
+
 }
