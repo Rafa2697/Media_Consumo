331602582bbda446e37385ad7faecea69354877a
Rafael <Rafael.mattos468@gmail.com>
2024-03-15 17:54:17 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,4 @@
-// function abriPage(a){
-//     let localPag = document.querySelector('body')
-//     let pag = new XMLHttpRequest()
 
-//     pag.onreadystatechange = () => {
-//         if(pag.readyState == 4 && pag.status == 200){
-//             localPag.innerHTML = pag.response
-//         }
-//     }
-
-//     pag.open('GET', `../${a}.html`)
-//     pag.send()
-// }
 
 
 let pages = ['app2.html', 'app1.html'];
@@ -34,22 +22,6 @@ document.querySelector('#botao').addEventListener('click', function () { // ness
 
 
 
-
-// document.querySelector('#botao1').addEventListener('click', function() {
-//     fetch('app2.html')
-//         .then(response => response.text())
-//         .then(data => {
-//             let parser = new DOMParser();
-//             let htmlDocument = parser.parseFromString(data, 'text/html');
-//             let divContent = htmlDocument.querySelector('body').innerHTML;
-//             document.querySelector('body').innerHTML = divContent;
-//         })
-//         .catch(error => {
-//             console.error('Erro:', error);
-//         });
-// });
-
-
 function calcGasto() {
     const percurso = Number(document.getElementById('km').value)
     const mediaVeiculo = Number(document.getElementById('media-veiculo').value)
@@ -67,18 +39,30 @@ function calcGasto() {
     }
 
 }
-window.onload = function() {
-    // Verifica se existe média salva no localStorage
+
+
+// funções app1
+
+window.onload = function () {
+    // Verifica se existe média, km e litros salvos no localStorage
     const mediaSalva = localStorage.getItem('mediaConsumo');
-    if (mediaSalva) {
-        document.getElementById('result-ult-media').innerHTML = `Última média de consumo: ${mediaSalva} Quilômetros por Litro`;
+    const kmSalvo = localStorage.getItem('km');
+    const litrosSalvo = localStorage.getItem('litros');
+    if (mediaSalva && kmSalvo && litrosSalvo) {
+        document.getElementById('result-ult-media').innerHTML = `Última média: <span>${mediaSalva}</span> Quilômetros por Litro, com <span>${kmSalvo}</span> km e <span>${litrosSalvo}</span> litros.`;
     }
 }
 function calcMedia() {
     const km = Number(document.getElementById('km').value)
     const litros = Number(document.getElementById('litros').value)
     let media = km / litros;
-
+   // Verifica se existe média, km e litros salvos no localStorage
+   const mediaSalva = localStorage.getItem('mediaConsumo');
+   const kmSalvo = localStorage.getItem('km');
+   const litrosSalvo = localStorage.getItem('litros');
+   if (mediaSalva && kmSalvo && litrosSalvo) {
+       document.getElementById('result-ult-media').innerHTML = `Última média de consumo: ${mediaSalva} Quilômetros por Litro, com ${kmSalvo} km e ${litrosSalvo} litros.`;
+   }
 
     if (isNaN(media)) {
         document.getElementById('result-media').innerHTML = `Prencha os campos para ter a média de consumo`
@@ -86,9 +70,10 @@ function calcMedia() {
     } else {
         // Salva a média no localStorage
         localStorage.setItem('mediaConsumo', media.toFixed(2));
+        localStorage.setItem('km', km);
+        localStorage.setItem('litros', litros);
         document.getElementById('result-media').innerHTML = `<span>${media.toFixed(2)}</span> Quilômetros por Litro`
         document.querySelector('body').style.background = `darkblue`
     }
 }
 
-  
